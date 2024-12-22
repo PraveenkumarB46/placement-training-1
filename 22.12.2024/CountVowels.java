821ed0b4fee992f61c630cabfa316878fa393b4c
PraveenkumarB46 <151357767+PraveenkumarB46@users.noreply.github.com>
2024-12-22 21:24:56 +0530

    Create CountVowels.java

---
+++ 22.12.2024/CountVowels.java
@@ -0,0 +1,12 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "Hello World"; // String to count vowels
+        int count = 0;
+        for (char ch : str.toLowerCase().toCharArray()) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
