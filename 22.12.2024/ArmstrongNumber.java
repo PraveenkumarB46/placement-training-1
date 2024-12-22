e2d6e124093e15bc356d8a3358a4239d68ad78d0
PraveenkumarB46 <151357767+PraveenkumarB46@users.noreply.github.com>
2024-12-22 21:24:01 +0530

    Create ArmstrongNumber.java

---
+++ 22.12.2024/ArmstrongNumber.java
@@ -0,0 +1,16 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num = 153; // Number to check
+        int original = num, sum = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number");
+        } else {
+            System.out.println(original + " is not an Armstrong number");
+        }
+    }
+}
