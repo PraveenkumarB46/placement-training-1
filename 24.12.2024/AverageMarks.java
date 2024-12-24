8dad16a12b67f6defef6977393d3e492909f9752
PraveenkumarB46 <151357767+PraveenkumarB46@users.noreply.github.com>
2024-12-24 17:43:31 +0530

    AverageMarks.java

---
+++ 24.12.2024/AverageMarks.java
@@ -0,0 +1,22 @@
+v=import java.util.Scanner;
+
+public class AverageMarks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = scanner.nextInt();
+
+        double totalMarks = 0;
+      
+        for (int i = 1; i <= numberOfStudents; i++) {
+            System.out.print("Enter marks for student " + i + ": ");
+            double marks = scanner.nextDouble();
+            totalMarks += marks; // Add marks to total
+        }
+        double averageMarks = totalMarks / numberOfStudents;
+
+        // Display the average marks
+        System.out.printf("The average marks of the class is: %.2f%n", averageMarks);
+        scanner.close();
+    }
+}
